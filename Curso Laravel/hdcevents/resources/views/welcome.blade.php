18b2da98c868b6f7b51838bf69fb519f0255d24a
isaquexavier1155 <isaque.ixs@gmail.com>
2023-11-29 08:30:15 -0300

    Adição de campo de data no banco de dados

--- Curso Laravel/hdcevents/resources/views/welcome.blade.php
+++ Curso Laravel/hdcevents/resources/views/welcome.blade.php
@@ -18,13 +18,16 @@
           <div class="card col-md-3">
               <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
               <div class="card-body">
-                  <p class="card-date">25/12/2023</p>
+                  <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
                   <h5 class="card-title">{{ $event->title }}</h5>
                   <p class="card-participants">X Participantes</p>
                   <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber Mais...</a>
               </div>
           </div>
           @endforeach
+          @if(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
+          @endif
       </div>
     </div>
 
