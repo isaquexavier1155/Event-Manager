6d98a0cc2257af97688766415efd429c1bb5c81c
isaquexavier1155 <isaque.ixs@gmail.com>
2023-11-29 09:27:59 -0300

    Implementação de pacotes e classes para autenticação via comando

--- Curso Laravel/hdcevents/routes/web.php
+++ Curso Laravel/hdcevents/routes/web.php
@@ -20,3 +20,12 @@ Route::get('/events/contact', [EventController::class, 'contact']);
 // Rout::get('/contact', function () {
 //     return view('contact');
 // });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
