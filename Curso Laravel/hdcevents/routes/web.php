777f1baea080ca6bacf1e4a4f2252f6c819fa7e8
isaquexavier1155 <isaque.ixs@gmail.com>
2023-12-03 16:47:33 -0300

    Criação de dashboard e view de eventos por usuário

--- Curso Laravel/hdcevents/routes/web.php
+++ Curso Laravel/hdcevents/routes/web.php
@@ -20,12 +20,15 @@ Route::get('/events/contact', [EventController::class, 'contact']);
 // Rout::get('/contact', function () {
 //     return view('contact');
 // });
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+
+
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
